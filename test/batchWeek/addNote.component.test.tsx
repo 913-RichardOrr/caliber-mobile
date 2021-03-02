5b4a3416db5a91bd62690dc7cf739850f18747c9
Janine Monique Geraldizo <jm.geraldizo@gmail.com>
2021-03-02 10:26:24 -0500

    fixing code smells?

--- test/batchWeek/addNote.component.test.tsx
+++ test/batchWeek/addNote.component.test.tsx
@@ -1,19 +1,15 @@
 /**
  * @jest-environment jsdom
  */
-import { shallow, ShallowWrapper, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import 'react-native';
 import 'jest-enzyme';
 import '@testing-library/jest-dom';
 import 'enzyme-adapter-react-16';
 
-import AddNoteComponent from '../../batchWeek/AddNoteComponent';
+
 import { Input } from 'react-native-elements';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider, useSelector, useDispatch } from 'react-redux';
-import store from '../../store/store';
-import BatchPageComponent from '../../batchPage/BatchPageComponent';
 
 test('test adding an overall note', () => {
   const mockChange = jest.fn();
