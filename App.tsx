3adcd9df5af8a941f5ba698984527517d7cd5ccf
kathrync23 <kathrynclark16@augustana.edu>
2021-03-02 12:58:49 -0600

    removed code smells

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import store from './store/store';
 import RouterComponent from './router/router.component';
-import AuthWrapper from './user/AuthWrapper';
 
 export default function App() {
   return (
--- test/auth/LogoutComponent.test.tsx
+++ test/auth/LogoutComponent.test.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { configure, shallow, render, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import LogoutComponent from '../../user/Logout';
-import {logout} from './functions';
+import { logout } from './functions';
 
 jest.mock('./functions', () => {
-    signOut: {
-        logout: jest
-        .fn()
-        .mockImplementation( () => 
-            Promise.resolve(true)
-        )
-    } 
-})
+  signOut: {
+    logout: jest.fn().mockImplementation(() => Promise.resolve(true));
+  }
+});
 
 test('onPress', () => {
-        const wrapper = shallow(
-        <LogoutComponent/>
-    );
+  const wrapper = shallow(<LogoutComponent />);
 
-    wrapper.simulate('press');
-    expect(logout);
-
-})
+  wrapper.simulate('press');
+  expect(logout);
+});
