24d4e33beb7fefdb05cb42e52b94e9e8be99be47
Jeff Graham <jeffery.lemar.graham@gmail.com>
2021-03-02 11:17:29 -0500

    remove unused & duplicate imports

--- batches/BatchListComponent.tsx
+++ batches/BatchListComponent.tsx
@@ -7,7 +7,6 @@ import { FlatList, TextInput } from 'react-native-gesture-handler';
 import { style } from '../global_styles';
 import { RootState } from '../store/store';
 import { changeBatch } from '../store/actions';
-import { style } from '../global_styles';
 
 /**
  * @typedef {Object} Props
@@ -15,8 +14,8 @@ import { style } from '../global_styles';
  * @property {*} route
  */
 interface Props {
-	navigation: any;
-	route: any;
+  navigation: any;
+  route: any;
 }
 
 /**
@@ -25,8 +24,8 @@ interface Props {
  * @property {string} info
  */
 interface VisibleBatch {
-	index: number;
-	info: string;
+  index: number;
+  info: string;
 }
 
 /**
@@ -35,167 +34,167 @@ interface VisibleBatch {
  * @returns {JSX}
  */
 export default function BatchListComponent({ navigation, route }: Props) {
-	const dispatch = useDispatch();
-	const user = useSelector((state: RootState) => state.userReducer.user);
-	const batches = useSelector((state: RootState) => state.batchReducer.batches);
-	const [visibleBatches, setVisible] = useState<VisibleBatch[]>([]);
-	const [reset, setReset] = useState(false);
-	const [query, setQuery] = useState('');
-	const keyExtractor = (item: object, index: number) => {
-		return index.toString();
-	};
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.userReducer.user);
+  const batches = useSelector((state: RootState) => state.batchReducer.batches);
+  const [visibleBatches, setVisible] = useState<VisibleBatch[]>([]);
+  const [reset, setReset] = useState(false);
+  const [query, setQuery] = useState('');
+  const keyExtractor = (item: object, index: number) => {
+    return index.toString();
+  };
 
-	const year = route.params.year;
-	const quarter = route.params.quarter;
+  const year = route.params.year;
+  const quarter = route.params.quarter;
 
-	// Filters based on the year and quarter, then displays different data based on the user's role(s)
-	useEffect(() => {
-		const batchesInQuarter = batches.filter((batch) => {
-			if (
-				year == checkYear(batch.startDate) &&
-				(quarter == 'All Quarters' || quarter == checkQuarter(batch.startDate))
-			) {
-				return batch;
-			}
-		});
+  // Filters based on the year and quarter, then displays different data based on the user's role(s)
+  useEffect(() => {
+    const batchesInQuarter = batches.filter((batch) => {
+      if (
+        year == checkYear(batch.startDate) &&
+        (quarter == 'All Quarters' || quarter == checkQuarter(batch.startDate))
+      ) {
+        return batch;
+      }
+    });
 
-		const visible = batchesInQuarter.map((batch, index) => {
-			return {
-				index,
-				info:
-					user.role.ROLE_QC === true || user.role.ROLE_VP === true
-						? `${batch.trainerFirstName + ' ' + batch.trainerLastName}\n${
-								batch.skill
-						  }\n${batch.startDate}`
-						: `${batch.skill}\n${batch.startDate}`,
-			};
-		});
-		setVisible(visible);
-		setReset(false);
-	}, [batches, reset === true]);
+    const visible = batchesInQuarter.map((batch, index) => {
+      return {
+        index,
+        info:
+          user.role.ROLE_QC === true || user.role.ROLE_VP === true
+            ? `${batch.trainerFirstName + ' ' + batch.trainerLastName}\n${
+                batch.skill
+              }\n${batch.startDate}`
+            : `${batch.skill}\n${batch.startDate}`,
+      };
+    });
+    setVisible(visible);
+    setReset(false);
+  }, [batches, reset === true]);
 
-	// Upon selection, updates the state with a chosen batch
-	// The navigator's destination is to be replaced in code after determining the next component in line
-	/**
-	 * Selects batch and navigates to BatchDetail screen
-	 * @param {string} index
-	 */
-	function handleBatchSelect(index: string) {
-		dispatch(changeBatch(batches[Number(index)]));
-		navigation.navigate('BatchDetail');
-	}
+  // Upon selection, updates the state with a chosen batch
+  // The navigator's destination is to be replaced in code after determining the next component in line
+  /**
+   * Selects batch and navigates to BatchDetail screen
+   * @param {string} index
+   */
+  function handleBatchSelect(index: string) {
+    dispatch(changeBatch(batches[Number(index)]));
+    navigation.navigate('BatchDetail');
+  }
 
-	/**
-	 * Transforms startDate into 4-digit year
-	 * @param {string} date
-	 */
-	function checkYear(date: string) {
-		return Number(date.slice(0, 4));
-	}
+  /**
+   * Transforms startDate into 4-digit year
+   * @param {string} date
+   */
+  function checkYear(date: string) {
+    return Number(date.slice(0, 4));
+  }
 
-	/**
-	 * Checks a provided date to see which quarter it's in and returns a string representing the quarter
-	 * @param {string} date
-	 */
-	function checkQuarter(date: string) {
-		const month: number = Number(date.slice(5, 7));
-		switch (month) {
-			case 1:
-			case 2:
-			case 3:
-				return 'Q1';
-			case 4:
-			case 5:
-			case 6:
-				return 'Q2';
-			case 7:
-			case 8:
-			case 9:
-				return 'Q3';
-			case 10:
-			case 11:
-			case 12:
-				return 'Q4';
-		}
-	}
+  /**
+   * Checks a provided date to see which quarter it's in and returns a string representing the quarter
+   * @param {string} date
+   */
+  function checkQuarter(date: string) {
+    const month: number = Number(date.slice(5, 7));
+    switch (month) {
+      case 1:
+      case 2:
+      case 3:
+        return 'Q1';
+      case 4:
+      case 5:
+      case 6:
+        return 'Q2';
+      case 7:
+      case 8:
+      case 9:
+        return 'Q3';
+      case 10:
+      case 11:
+      case 12:
+        return 'Q4';
+    }
+  }
 
-	/**
-	 * Display a selectable batch
-	 * @param {*} params
-	 * @returns {JSX}
-	 */        
-	const batchCard = (params: any) => {
-		return (
-			<Pressable onPress={() => handleBatchSelect(params.item.index)}>
-				<Card>
-					<Text>{params.item.info}</Text>
-				</Card>
-			</Pressable>
-		);
-	};
+  /**
+   * Display a selectable batch
+   * @param {*} params
+   * @returns {JSX}
+   */
+  const batchCard = (params: any) => {
+    return (
+      <Pressable onPress={() => handleBatchSelect(params.item.index)}>
+        <Card>
+          <Text>{params.item.info}</Text>
+        </Card>
+      </Pressable>
+    );
+  };
 
-	/**
-	 * Filter batch cards based on new text input; sets query and visible states;
-	 * @param {string} text
-	 */
-	const handleSearch = (text: string) => {
-		let visible: VisibleBatch[] = [];
-		if (query.length > text.length) {
-			visible = batches.map((batch, index) => {
-				return {
-					index,
-					info:
-						user.role.ROLE_QC === true || user.role.ROLE_VP === true
-							? `${batch.name} ${batch.skill} ${batch.startDate} - ${
-									batch.trainerFirstName + ' ' + batch.trainerLastName
-							  }`
-							: `${batch.name} ${batch.skill} ${batch.startDate}`,
-				};
-			});
+  /**
+   * Filter batch cards based on new text input; sets query and visible states;
+   * @param {string} text
+   */
+  const handleSearch = (text: string) => {
+    let visible: VisibleBatch[] = [];
+    if (query.length > text.length) {
+      visible = batches.map((batch, index) => {
+        return {
+          index,
+          info:
+            user.role.ROLE_QC === true || user.role.ROLE_VP === true
+              ? `${batch.name} ${batch.skill} ${batch.startDate} - ${
+                  batch.trainerFirstName + ' ' + batch.trainerLastName
+                }`
+              : `${batch.name} ${batch.skill} ${batch.startDate}`,
+        };
+      });
 
-			visible = visible.filter((batch) =>
-				batch.info.toLowerCase().includes(text.toLowerCase())
-			);
-		} else {
-			visible = visibleBatches.filter((batch) =>
-				batch.info.toLowerCase().includes(text.toLowerCase())
-			);
-		}
-		setQuery(text);
-		setVisible(visible);
-	};
+      visible = visible.filter((batch) =>
+        batch.info.toLowerCase().includes(text.toLowerCase())
+      );
+    } else {
+      visible = visibleBatches.filter((batch) =>
+        batch.info.toLowerCase().includes(text.toLowerCase())
+      );
+    }
+    setQuery(text);
+    setVisible(visible);
+  };
 
-	// Displays a list of batches based on filters
-	return (
-		<View>
-			<Text style={style.subheading}>
-				{year + ' > ' + route.params.quarter}
-			</Text>
-			<View style={style.container}>
-				<TextInput
-					style={{ width: 300, borderWidth: 1, borderRadius: 10 }}
-					value={query}
-					onChangeText={(text) => {
-						handleSearch(text);
-					}}
-				/>
-				<Button
-					color="#F26925"
-					title="Clear"
-					onPress={() => {
-						setQuery('');
-						setReset(true);
-					}}
-				/>
-			</View>
-			<Text style={{ margin: 10 }}>Select Batch:</Text>
-			{year !== null && (
-				<FlatList
-					data={visibleBatches}
-					renderItem={batchCard}
-					keyExtractor={keyExtractor}
-				/>
-			)}
-		</View>
-	);
+  // Displays a list of batches based on filters
+  return (
+    <View>
+      <Text style={style.subheading}>
+        {year + ' > ' + route.params.quarter}
+      </Text>
+      <View style={style.container}>
+        <TextInput
+          style={{ width: 300, borderWidth: 1, borderRadius: 10 }}
+          value={query}
+          onChangeText={(text) => {
+            handleSearch(text);
+          }}
+        />
+        <Button
+          color='#F26925'
+          title='Clear'
+          onPress={() => {
+            setQuery('');
+            setReset(true);
+          }}
+        />
+      </View>
+      <Text style={{ margin: 10 }}>Select Batch:</Text>
+      {year !== null && (
+        <FlatList
+          data={visibleBatches}
+          renderItem={batchCard}
+          keyExtractor={keyExtractor}
+        />
+      )}
+    </View>
+  );
 }
