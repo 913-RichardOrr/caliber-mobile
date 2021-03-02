83237469e476ec4a2c143b7325479803d28e009b
Jung Woo <michaelwoo921@gmail.com>
2021-03-02 08:15:36 -0800

    created TempPartReport.tsx

--- App.tsx
+++ App.tsx
@@ -5,13 +5,12 @@ import { View } from 'react-native';
 import store from './store/store';
 import RouterComponent from './router/router.component';
 import style from './global_styles';
+import TempPartReport from './TempPartReport';
 
 export default function App() {
   return (
       <Provider store={store}>
-          <NavigationContainer>
-            <RouterComponent />
-          </NavigationContainer>
+        <TempPartReport />
       </Provider>
   );
 }
