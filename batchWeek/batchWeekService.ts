3a55bcbc6a3a98fd2e0fd8c88e5a60bd8d01436b
Lilith Moore <jessejm123@gmail.com>
2021-03-02 18:35:40 -0500

    Small changes in batchweek service file

--- batchWeek/batchWeekService.ts
+++ batchWeek/batchWeekService.ts
@@ -4,28 +4,36 @@ import QcWeek from './QcWeek';
 class BatchWeekService {
     private URI: string;
     constructor() {
-        this.URI ="https://x9ofuwde2l.execute-api.us-east-1.amazonaws.com/default";
+        this.URI ="https://d3e1hb8u20.execute-api.us-east-1.amazonaws.com/default/";
     }
 
     private getConfig(token: string): AxiosRequestConfig {
         return { 
-            headers: {'Authorization': `Bearer ${token}`}
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
         };
     }
 
     // get all week objects for a specific batch
     getWeeksByBatchId(token: string, batchid: string): Promise<QcWeek[]> {
-        return axios.get(this.URI + `qc/batches/${batchid}/weeks`, this.getConfig(token)).then(result => result.data);
+        return axios.get(this.URI + `qc/batches/${batchid}/weeks`, this.getConfig(token)).then(result => result.data).catch((err) => {
+            console.error(err);
+        });
     }
 
     // add new qc_week to the qc_week table for /batches/{batchId}/weeks
-    addWeek(token: string, qw: QcWeek): Promise<null> {
-        return axios.post(this.URI + `qc/batches/${qw.batchid}/weeks`, qw, this.getConfig(token)).then(result => null);
+    addWeek(token: string, qw: QcWeek): Promise<any> {
+        return axios.post(this.URI + `qc/batches/${qw.batchid}/weeks`, qw, this.getConfig(token)).then().catch((err) => {
+            console.error(err);
+        });
     }
 
      // update the overall note and technical status for a week
-    updateFeedback(token: string, qw: QcWeek): Promise<null> {
-        return axios.post(this.URI + `qc/batches/${qw.batchid}/weeks/${qw.weeknumber}`, qw, this.getConfig(token)).then(result => null);
+    updateFeedback(token: string, qw: QcWeek): Promise<any> {
+        return axios.post(this.URI + `qc/batches/TR-1004/weeks/${qw.weeknumber}`, qw, this.getConfig(token)).then().catch((err) => {
+            console.error(err);
+        });
     }
 
 }
